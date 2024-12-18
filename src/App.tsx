024cd3835a4e367d0ab3f3f635ab475c5bb134d3
Sacha MORLAT <sachamorlat@gmail.com>
2024-12-18 19:54:40 +0100

    add login on app

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,7 @@ function App() {
                     <Route path="/login" element={<Login />} />
                     <Route path="/register" element={<Register />} />
                     <Route path="/dashboard" element={<Dashboard />} />
-                    <Route path="/" element={<Navigate to="/dashboard" />} />
+                    <Route path="/" element={<Navigate to="/login" />} />
                 </Routes>
             </BrowserRouter>
         </AuthProvider>
